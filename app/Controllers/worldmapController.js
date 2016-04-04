a6096c559b78b88ebde2111c6432a4f377ada41a
Kajaffie <Jafeth.vanElten@hotmail.com>
2016-04-04 12:24:27 +0200

    Improved map colours. Added legend to map. Added interface for country data table underneath the worldmap.

--- app/Controllers/worldmapController.js
+++ app/Controllers/worldmapController.js
@@ -1,6 +1,7 @@
 app.controller('worldmapController', ['$location', 'worldmapService', function($location, worldmapService) {
 	var worldMapController = this;
 	var manager = $location.search().managerID; 
+	var countries;
 	
 	worldMapController.getWorldmap = function() {
 		worldmapService.getCountries(manager).success(function(data) {
@@ -10,25 +11,73 @@ app.controller('worldmapController', ['$location', 'worldmapService', function($
 					countryData[value.landIso] = value.status;
 				});
 			
-			console.log(countryData);
-			
-			$('#world-map').vectorMap({
+			worldMapController.createWorldMap(countryData);
+			worldMapController.countries = data;
+		});
+		
+	};
+	
+	worldMapController.createWorldMap = function(countryData)
+	{
+		$('#world-map').vectorMap({
 				map: 'world_mill',
 				series: {
 					regions: [{
 						values: countryData,
 						scale: ['#888888', '#FFFFFF', '#BBFFBB', '#66FF66', '#00FF00'],
-						normalizeFunction: 'linear'
+						normalizeFunction: 'linear',
+						legend: {
+							vertical: true,
+							title: 'Legenda',
+							labelRender: function(v){
+							return {
+								0: 'Niet Beschikbaar.',
+								1: 'Niets gewonnen.',
+								2: 'Doel behaald.',
+								3: 'Beker gewonnen.',
+								4: 'Kampioenschap gewonnen!'
+								}[v];
+							}
+						}
 					}]
 				},
-				backgroundColor: '#0000FF'
+				backgroundColor: '#016CD5',
+				onRegionTipShow: function(e, el, code){
+					var flavourText = worldMapController.getFlavourText(countryData, code);
+					el.html(el.html()+' (Status: - ' + flavourText + ')');
+				}
 			});
-		});
-	};
+	}
 	
-	function testMethod(){
-		console.log("Hoera");
-	};
+	worldMapController.getFlavourText = function(countryData, code)
+	{
+		var flavourText;
+		if(countryData[code] == 0)
+			{
+				flavourText = "niet beschikbaar.";
+			}
+			else if(countryData[code] == 1)
+			{
+				flavourText = "Niets gewonnen.";
+			}
+			else if(countryData[code] == 2)
+			{
+				flavourText = "Doel behaald.";
+			}
+			else if(countryData[code] == 3)
+			{
+				flavourText = "Beker gewonnen.";
+			}
+			else if(countryData[code] == 4)
+			{
+				flavourText = "Kampioenschap gewonnen!";
+			}
+			else
+			{
+				flavourText = "Unknown."
+			}
+		return flavourText;
+	}
 	
 	if(manager) {
 		worldMapController.getWorldmap();
