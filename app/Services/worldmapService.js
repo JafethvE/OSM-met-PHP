44b6f19f29a68c1c467d8de24e404b4fd0964d89
Mathilde <12118605@student.hhs.nl>
2016-04-04 14:18:54 +0200

    landjes

--- app/Services/worldmapService.js
+++ app/Services/worldmapService.js
@@ -1,5 +1,5 @@
 app.service('worldmapService', ['$http', function($http) {
 	this.getCountries = function(manager) {
-		return $http.post("http://localhost/OSM-met-PHP/api/worldmap.php", {manager: manager});
+		return $http.post("http://localhost/osm/api/worldmap.php", {manager: manager});
 	};
 }]);
